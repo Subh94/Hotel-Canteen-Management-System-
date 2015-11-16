f8bc44966630be258d2be7473871e0e0520ee1e3
Subhash Kumar <agarwal1994@live.in>
2015-11-16 19:59:24 +0530

    restaurent File added to git

---
+++ Restaurant/canteen/access-denied.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Hotel Grand: Access denied</title>
+
+    <link href="css/css1.css" rel="stylesheet">
+
+    <link href="css/css2.css" rel="stylesheet">
+  </head>
+
+  <body>
+  
+    <div class="brand">Hotel Grand</div>
+    <div class="address-bar">near bus stand |  Mandi , Himachal Pradesh(H.P) 175001 | 9805343650</div>
+    <nav class="navbar navbar-default" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="">Hotel Grand</a>
+        </div>
+
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="unlogin-foodzone.php">Food Zone</a></li>
+            <li><a href="specialdeals.php">Special Deals</a></li>
+            <li><a href="member-index.php">My Account</a></li>
+            <li><a href="contactus.php">Contact Us</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <div class="row">
+        <div class="box">
+          <div class="col-lg-12 text-center">
+            <div id="carousel-example-generic" class="carousel slide">
+
+<div id="center">
+  <h1>Access Denied</h1>
+  <div>
+<div class="error">Access Denied!</div>
+  <p>You don't have access to this page. <a href="login-register.php">Click Here</a> to login first or register for free. The registration won't take long time</p>
+  </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <p>Copyright &copy;Friends &  Company 2013</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <script src="js/jquery1.js"></script>
+    <script src="js/jquery2.js"></script>
+    <script>
+    $('.carousel').carousel({
+    interval: 5000
+    })
+  </script>
+</body>
+</html>
